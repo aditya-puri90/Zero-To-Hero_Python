5f73c7c441f3e7e905167cd9328d1a3aff30014b
aditya puri <puriaditya90@gmail.com>
2026-02-13 22:29:47 +0530

    Add Addition.py and Division.py

---
+++ Basics/Addition.py
@@ -0,0 +1,6 @@
+# Write a python programm to do arithmetic operation Addition
+
+num1=float(input("Enter the number:"))
+num2=float(input("Enter the number:"))
+sum=num1+num2
+print(f"sum:{num1}+{num2}={sum}")
---
+++ Basics/Division.py
@@ -0,0 +1,9 @@
+# Write the python programm to do arithmetic operation Division
+
+num1=float(input("Enter the num1:"))
+num2=float(input("Enter the num2:"))
+if num2==0:
+    print("Error:Division by zero is not allowed")
+else:
+    div=num1/num2
+    print(f"Division:{num1}/{num2}={div}")
