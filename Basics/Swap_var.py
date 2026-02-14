ef038b4f49bb1c0e82047e182e29bcc4c7ed2b5e
aditya puri <puriaditya90@gmail.com>
2026-02-14 21:55:44 +0530

    Swaping two values

---
+++ Basics/Swap_var.py
@@ -0,0 +1,10 @@
+# write a python programm to swap two variables
+
+a=input("enter the value of first variable(a):")
+b=input("enter the value of second variable(b):")
+print(f"original value={a},{b}:")
+
+temp=a
+a=b
+b=temp
+print(f"Swapped value:a={a},b={b}")
