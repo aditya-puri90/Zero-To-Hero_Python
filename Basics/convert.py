9e00492b9a360df9ee45c50969d8b4cd621aee54
aditya puri <puriaditya90@gmail.com>
2026-02-16 21:55:23 +0530

    conversion of celsius to fahrenheit

---
+++ Basics/convert.py
@@ -0,0 +1,7 @@
+# Write a python programm to convert the celsius to fahrenheit
+
+celsius=float(input("Enter the tempreture in celsius:"))
+
+fahrenheit=(celsius*9/5)+31
+
+print(f"{celsius} degree celsius is equal to {fahrenheit}  fahrenheit degree :")
