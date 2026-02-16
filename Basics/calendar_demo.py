a81960a5ce94bdfa5ceeae6afe260ccb56fdba8c
Aditya Puri <puriaditya90@gmail.com>
2026-02-16 22:06:10 +0530

    Clarify script purpose with a comment
    Added a comment to clarify the purpose of the script.

--- Basics/calendar_demo.py
+++ Basics/calendar_demo.py
@@ -1,6 +1,8 @@
 """
 Script to display the calendar for a given year and month.
 """
+
+# Write a python programm to print the calendar
 import calendar
 year=int(input("Enter the year:"))
 month=int(input("Enter the month:"))
