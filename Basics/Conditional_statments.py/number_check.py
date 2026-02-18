548a178e60ff8b75516f00266c6784d3e6f406b7
aditya puri <puriaditya90@gmail.com>
2026-02-18 22:45:02 +0530

    Add Basics/Conditional_statments.py/number_check.py

---
+++ Basics/Conditional_statments.py/number_check.py
@@ -0,0 +1,9 @@
+# Write the python proggram to check whether the given number is positive, negative, or zero.
+
+number1=float(input("Enter the number:"))
+if number1 > 0:
+    print("The number is positive")
+elif number1 < 0:
+    print("The number is negative")
+else:
+    print("The number is zero")
