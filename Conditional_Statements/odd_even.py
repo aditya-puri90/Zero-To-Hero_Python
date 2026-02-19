34be676e476b37e5c1d92788237a9b1bcf3e37b7
aditya puri <puriaditya90@gmail.com>
2026-02-19 23:37:07 +0530

    Add odd_even.py

---
+++ Conditional_Statements/odd_even.py
@@ -0,0 +1,7 @@
+# write a python programm to check the number is odd or even
+
+number1=float(input("Enter the number:"))
+if number1 % 2==0:
+    print("The number is even :",number1)
+else:
+    print("The number is odd :",number1)
