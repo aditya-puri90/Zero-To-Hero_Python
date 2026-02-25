872d37cb6de836d27eb01918b297bf213ccc2bde
aditya puri <puriaditya90@gmail.com>
2026-02-25 23:14:07 +0530

    Add number_conversion.py

---
+++ Conditional_Statements/number_conversion.py
@@ -0,0 +1,7 @@
+# Write a python programm to convert decimal to binary, octal and hexadecimal.
+
+decimal= int(input("Enter the decimal number:"))
+print("The decimal value of", decimal, "is:")
+print("Binary:", bin(decimal))
+print("Octal:", oct(decimal))
+print("Hexadecimal", hex(decimal))
